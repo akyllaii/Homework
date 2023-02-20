b1f8059c34c19028cee92b1125fbdc135898765a
akyllaii <omurbekovaakylai@gmail.com>
2023-02-20 21:07:21 +0600

    7dz

---
+++ main.js
@@ -0,0 +1,98 @@
+// let a = [1, 2, 3, 4, 5]
+// for (let i = 0; i <= a.length; i++) {
+//     console.log(a[i])
+// }
+
+// let a = [1, 2, 3, 4, 5]
+// let result = 0
+// for (let i = 0; i < a.length; i++) {
+//     result += a[i];
+// }
+// console.log(result)
+
+// let i = 0
+// while (i < 3) {
+//     console.log(`number ${i}!`)
+//     i++
+// }
+
+// let a = [-2, 7, 9, 5, 8, 1, 12, 15, 17,3]
+// for (let i = 0; i <= a.length; i++) {
+//     if (a[i] > 0 && a[i] < 10) {
+//         console.log(a[i])
+//     }
+// }
+
+// for (let a = 2; a <= 10; a++) {
+//     if (a % 2 === 0) {
+//         console.log(a)
+//     }
+// }
+
+// let result = 1
+// for (let i = 0; i <= 100; i++) {
+//     result += i
+//  }
+// console.log(result)
+
+
+// let cars = ['BMW', 'Mercedes', 'Opel', 'Mazda', 'FIAT', 'Ferarri', 'Reno']
+// let bigCars = []
+// for (let i = 0; i < cars.length; i++) {
+//     if (cars[i] === cars[i].toUpperCase()) {
+//         bigCars = [...bigCars, cars[i].toUpperCase()]
+//     }
+// }
+// console.log(bigCars)
+
+// let arr = [2,3,4,5,6,7]
+//  for (let i = 0; i <= arr.length; i++) {
+//      if (arr[i] !== 5) {
+//          console.log(arr[i] * 2)
+//      } else {
+//          console.log(arr[i])
+//      }
+//  }
+
+
+// let str = [-2, 7, '9b', 'Vlad', 1, 12, 15, 'Ivan', 17, 3]
+// for (let i = 0; i < str.length; i++) {
+//     if (typeof str[i] === "string") {
+//         console.log(str[i])
+//     }
+// }
+
+// let days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+// for (let i = 0; i < days.length; i++) {
+//     if (days[i] === 'saturday' || days[i] === 'sunday') {
+//         console.log(days[i] = 'weekend')
+//     } else {
+//         console.log(days[i])
+//     }
+// }
+
+
+// let users = ['Наташа', 'Анастасия', 'Дмитрий', 'Максим']
+// for (let i = 0; i < users.length; i++) {
+//     delete users[0]
+//     console.log(users[i])
+// }
+
+
+ // let users = ['Наташа', 'Анастасия', 2, 'Максим', 4]
+ // for (let i = 0; i < users.length; i++) {
+ //     if (typeof users[i] === "string") {
+ //         console.log(users[i])
+ //     }
+ // }
+ //
+ // for (let i = 0; i < users.length; i++) {
+ //     if (typeof users[i] === "number") {
+ //         console.log(users[i])
+ //     }
+ // }
+
+
+// for (let i = 2; i <= 10; i++) {
+//     console.log(`5 * ${i} = ${5 * i}`)
+// }
