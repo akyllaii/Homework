1b185cdc68af393805a3dfacf409972a2a14384e
akyllaii <omurbekovaakylai@gmail.com>
2023-02-23 19:12:53 +0600

    7dz

---
+++ 7dz/main.js
@@ -0,0 +1,70 @@
+// let name = 'Анастасия'
+// name = 'Максим'
+// console.log(name)
+
+// let str = 'Я хочу быть большим и сильным'
+// console.log(str.length)
+
+// let a = '15'
+// let b = 15
+// let c = 10
+// console.log(a+b+c)
+// console.log(c+b+a)
+// потому что в первом варианте переменные b и с автоматически преобразуются в строки, следоваельно мы получаем 151510, а во втором варианте
+// сначала суммируются переменные b и с, только потом преобразуются в строки
+
+// let arr = [[1,2,3], [4,5,6], [7,8,9]]
+// console.log(arr[1][0])
+
+// let user = {
+//     name: 'Ivan',
+//     age: 35,
+//     gender: 'male',
+//     job: 'web-developer'
+// }
+// for (let key in user) {
+//     console.log(`Значением свойства ${key} является ${user[key]}`)
+// }
+
+// let surname = 'Серебров'
+// let arr = surname.split('')
+// arr.splice(7, 1, 'ф','ф')
+// arr = arr.join('')
+// console.log(arr)
+
+// let a = 'я люблю торт'
+// if (a.endsWith('торт')) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// let a = '10223040'
+// console.log(a.split('').reverse().join(''))
+
+// let age = 18
+// console.log(`Мне ${age} лет`)
+
+// let arr = "asdasdsadsadsadsadc"
+// console.log(arr.split(','))
+
+// let arr = [2,1,3,4,5]
+// arr = arr.sort()
+// arr.splice(3,0, 'a', 'b', 'c')
+// console.log(arr)
+
+// let city = 'Мой город самый лучший город в России'
+// console.log(city.indexOf('город'))
+
+// let a
+// let b
+// if (a > b) {
+//     console.log(a+b)
+// } else {
+//     console.log(a*b)
+// }
+
+// let arr = [12,'12',true,undefined,'Ivan', 35]
+// for (let key of arr) {
+//     console.log(`Массив сожержит в себе элемент ${key}`)
+// }
